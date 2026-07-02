5e9c9b57449a5ec5cf02d4297cfb6a91e1df6528
MOHIT KUMAR SHARMA <167669772+mohitnigas@users.noreply.github.com>
2026-07-02 22:48:27 +0530

    Delete prx directory

--- prx/music_library.py
+++
@@ -1,8 +0,0 @@
-music = {"nightcall": "https://www.youtube.com/watch?v=MV_3Dpw-BRY",
-          "cry":"https://youtu.be/3XqqkrJENB4?si=WL-wmSL82pymj7Du", 
-          "Apocalypse":"https://youtu.be/f8s6Wwn6UWY?si=Yu50gyK6LIuyiNDR", 
-          "SLOW DANCE IN THE DARK":"https://youtu.be/K3Qzzggn--s?si=rZ2ETUDhLNhwPBzn", 
-          "SUPERMAN":"https://youtu.be/K_8yRH2KPVo?si=NyrdhMfw0-QeIebc",
-          "PLAY DATE":"https://youtu.be/fG8nF53ceQc?si=sUUGrly2YQ6BdLTZ",
-          "i dont like":"https://youtu.be/2WcRXJ4piHg?si=u9FOJm8Y2B2o4ZVr",
-          "World starmoney":"https://youtu.be/sADmwWhU5ZM?si=dkCiOddAupKRPtn2"}
